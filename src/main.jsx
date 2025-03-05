70139d3f3d1283357ae93bd4eb72c8463a739316
Luís Henrique M. Troquato <96350540+LuqueMT@users.noreply.github.com>
2025-03-05 13:15:20 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppRoutes from "./Routes/routes"
-const _jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
 import "./App.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <AppRoutes/>
-  </StrictMode>,
-)
+  </StrictMode>
+);
