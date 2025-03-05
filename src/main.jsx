a5886ee3340778d9a7820a84ce686803e54876d4
Luís Henrique M. Troquato <96350540+LuqueMT@users.noreply.github.com>
2025-03-05 11:40:36 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import AppRoutes from "./Routes/routes"
+import "./App.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <AppRoutes/>
   </StrictMode>,
 )
